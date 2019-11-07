130ad4f935187b8e0451d1a3cab0099a9854f15f
GekOrdep <pedrovrcamargo@gmail.com>
2019-11-07 10:48:16 -0200

    Pacotes java entidades, servlets e utilidades adicionado

--- TP1Pedro2019/src/main/java/br/edu/iff/tp1pedro2019/entidades/Usuario.java
+++ TP1Pedro2019/src/main/java/br/edu/iff/tp1pedro2019/entidades/Usuario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.iff.tp1pedro2019;
+package br.edu.iff.tp1pedro2019.entidades;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
--- TP1Pedro2019/src/main/java/br/edu/iff/tp1pedro2019/servlets/MeuPrimeiroServlet.java
+++ TP1Pedro2019/src/main/java/br/edu/iff/tp1pedro2019/servlets/MeuPrimeiroServlet.java
@@ -1,3 +1,5 @@
+package br.edu.iff.tp1pedro2019.servlets;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
